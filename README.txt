f3c6928965f966e636e18d340ca96f29f45f3c8b
Anton Griev <anton@griev.ru>
2015-07-19 00:43:33 +0300

    Initial commit

---
+++ README.txt
@@ -0,0 +1,14 @@
+lanwell_new README
+==================
+
+Getting Started
+---------------
+
+- cd <directory containing this file>
+
+- $VENV/bin/python setup.py develop
+
+- $VENV/bin/initialize_lanwell_new_db development.ini
+
+- $VENV/bin/pserve development.ini
+
---
+++ lanwell_new/__init__.py
@@ -0,0 +1,26 @@
+from pyramid.config import Configurator
+from sqlalchemy import engine_from_config
+
+from .models import (
+    DBSession,
+    Base,
+    CompletedProject,
+    )
+
+
+def main(global_config, **settings):
+    """ This function returns a Pyramid WSGI application.
+    """
+    engine = engine_from_config(settings, 'sqlalchemy.')
+    DBSession.configure(bind=engine)
+    Base.metadata.bind = engine
+    config = Configurator(settings=settings)
+    config.include('pyramid_chameleon')
+    config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_route('home', '/')
+    config.include('pyramid_sacrud')
+    settings = config.registry.settings
+    settings['pyramid_sacrud.models'] = (('Group1', [CompletedProject]),
+                                     )
+    config.scan()
+    return config.make_wsgi_app()
